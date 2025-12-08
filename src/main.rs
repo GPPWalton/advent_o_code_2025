d8b829b8148c5db25f7ff61f5e41f26deb062141
Gabriel Walton <gabrielppwalton@gmail.com>
2025-12-08 20:50:16 +0000

    chore: restructuring to stretch my rust learning and provide a skeleton for future days

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,82 @@ use std::{env, ffi::OsString};
 use std::error::Error;
 use day_1::day_1_solution::{read_cmd_file};
 
-fn day_1 (method_flag: bool) {
-    //arrow that shows current dial position, starts ar 50
+enum Advent {
+    Day1,
+    Day2,
+    Day3,
+    Day4,
+    Day5,
+    Day6,
+    Day7,
+    Day8,
+    Day9,
+    Day10,
+    Day11,
+    Day12
+}
+fn day_1 () {
+    //current dial position, starts at 50
     let  starting_pos = 50;
-    let ctr = read_cmd_file(starting_pos, method_flag);
+    let ctr: i32;
 
+    //check value of flag
+    match  env::args_os().nth(2) {
+    //if no flag for day_1 run as normal.
+    None => {
+        //run without flag
+        ctr = read_cmd_file(starting_pos, false);
+    },
+    Some(argument) => { if argument == OsString::from("method")
+                        || argument == OsString::from("m")
+    {
+        ctr = read_cmd_file(starting_pos, true);
+    }
+    else{
+        panic!("2nd argument is invalid, if using new protocols, try 'method'/'m'")
+    }}
+    }
     println!("True code:    {}", ctr);
 }
+fn select_day (cmd: Advent) {
+    match cmd {
+        Advent::Day1 => day_1(),
+        Advent::Day2 =>{},
+        Advent::Day3 =>{},
+        Advent::Day4 =>{},
+        Advent::Day5 =>{},
+        Advent::Day6 =>{},
+        Advent::Day7 =>{},
+        Advent::Day8 =>{},
+        Advent::Day9 =>{},
+        Advent::Day10 =>{},
+        Advent::Day11 =>{},
+        Advent::Day12 =>{},
+    }
+}
 
-fn get_arg() -> Result<bool, Box<dyn Error>> {
+fn get_arg() -> Result<Advent, Box<dyn Error>> {
     //check first flag
     match env::args_os().nth(1) {
         //if no flaf, run as normal
-        None => Ok(false),
-        Some(argument)=> {if argument == OsString::from("method")
-                                    || argument == OsString::from("m")
-            {
-                //check value of flag
-               match  env::args_os().nth(2) {
-                None => Err(From::from("No value given for flag")),
-                Some(value) => {if value == OsString::from("0x434C49434B")
-                {
-                    Ok(true)
-                }
-                else{
-                    Err(From::from("Invalid method"))
-                }}
-               }
+        None => Err(From::from("No argument, please input a day")),
+        Some(argument)=> {
+            if argument == OsString::from("day_1") {
+                Ok(Advent::Day1)
             }
-        else{
-            Err(From::from("Invalid argument"))
-        }},
+            else if argument == OsString::from("day_2") {
+                Ok(Advent::Day2)
+            }
+            else{
+                Err(From::from("Invalid argument"))
+            }},
     }
 }
 fn main() {
-    let method_flag = match get_arg(){
-        Ok(flag) => flag,
+    let cmd_arg = match get_arg(){
+        Ok(cmd) => cmd,
         Err(err) => panic!("{:?}",err)
     };
     
-    day_1(method_flag)
+    select_day(cmd_arg);
 }
